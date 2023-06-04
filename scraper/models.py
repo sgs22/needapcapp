6dd74e9f012a835c5cc7867701c480ed7a12486a
sgs22 <samuel_smith@outlook.com>
2023-06-04 18:18:21 +0100

    add scraper app

---
+++ scraper/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class CurrysScraper:
+    def __init__(self):
+        self.base_url = 'https://www.currys.co.uk'
+        self.session = requests.Session()
+        self.session.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+    def search_products(self, search_query):
+        search_query = search_query.replace(' ', '+')
+        url = f"{self.base_url}/gbuk/search-keywords/xx_xx_xx_xx_xx/{search_query}/xx-criteria.html"
+
+        response = self.session.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        products = []
+
+        for product in soup.find_all('article', class_='product'):
+            name = product.find('strong', class_='productTitle').text.strip()
+            price = product.find('strong', class_='current-price').text.strip()
+
+            products.append({'name': name, 'price': price})
+
+        return products
+    
+    def get_product_details(self, product_code: str):
+        url = f'https://www.currys.co.uk/search?q={product_code}'
+
+        response = self.session.get(url)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        print(soup)
+        product = {}
+        product_name = soup.find('h1', class_='product-name').text.strip()
+        product_price = soup.find('span', class_='value').text.strip()
+        product.append({'name': product_name, 'price': product_price})
+        print(product)
+
+
+if __name__ == "__main__":
+    scraper = CurrysScraper()
+    scraper.get_product_details('359445')
