c134b16c99b8d0754ca548dc1651a07e9d9ad0a5
sgs22 <samuel_smith@outlook.com>
2023-11-07 00:13:34 +0000

    add model tests

--- app/tests.py
+++ app/tests.py
@@ -1,20 +1,30 @@
 from django.test import TestCase
 from django.urls import reverse
-from app.models import Quiz
+from app.models import Quiz, QuizResponse, User
 
 
 
-class NeedapcTestCase(TestCase):
+class AppViewsTest(TestCase):
+
     def test_index_response(self):
         index = self.client.get(reverse('index'))
         response = index.status_code
         self.assertEquals(response, 200)
 
 
+class QuizResponseModelTest(TestCase):
+    def setUp(self) -> None:
+        self.quiz = Quiz.objects.create(name='Test Quiz', description='Test Quiz Description')
+        self.user = User.objects.create_user('test_user', '', 'test_password')
+        self.quiz_response = QuizResponse.objects.create(quiz_id=self.quiz.pk, user_id=self.user.pk, response={'question_1': 'choice_1'})
 
-class NeedapcQuizModelTestCase(TestCase):
-    def setUp(self):
-        self.model_test_quiz_instance = Quiz.objects.create(name='Test Quiz', description='Test Quiz Description')
+    def test_quiz_response_has_user(self):
+        self.assertEquals(self.quiz_response.user.pk, 1)
 
-    def test_quiz_model_has_slug(self):
-        self.assertEquals(self.model_test_quiz_instance.slug, 'test-quiz')
+    def test_quiz_response_has_quiz(self):
+        self.assertEquals(self.quiz_response.quiz.pk, 1)
+
+    def text_quiz_response_response_is_valid_json(self):
+        self.assertEquals(self.quiz_response.response, {'question_1': 'choice_1'})
+
+    
