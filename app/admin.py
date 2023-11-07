166b20329d8484f02c49b9824623501a5b60048d
sgs22 <samuel_smith@outlook.com>
2023-11-07 00:12:44 +0000

    add admin register for models

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from app.models import Quiz, Question, Choice, QuizResponse
 
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(QuizResponse)
+from django.contrib import admin
+from app.models import Quiz, Question, Choice, QuizResponse
+
+for model in [Quiz, Question, Choice, QuizResponse]:
+    admin.site.register(model)
