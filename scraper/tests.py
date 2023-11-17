84b59f0f68bb97c0f124f8a319e013f49746e456
sgs22 <samuel_smith@outlook.com>
2023-11-17 00:54:34 +0000

    add basic tests

--- scraper/tests.py
+++ scraper/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import CurrysScraper, LaptopsDirectScraper
 
-# Create your tests here.
+
+class TestCurrysScraper(TestCase):
+    def setUp(self) -> None:
+        self.scraper = CurrysScraper()
+
+    def test_query_products(self):
+        products = self.scraper.query_product_segment()
+        print(products)
+        self.assertEquals(len(products) > 0, True)
+
+    def test_soup_session_valid(self):
+        session = self.scraper.get_session(self.scraper.base_url)
+        self.assertEquals(session.status_code, 200)
+
+
+class TestLaptopsDirectScraper(TestCase):
+    def setUp(self) -> None:
+        self.scraper = LaptopsDirectScraper()
+
+    def test_session_valid(self):
+        session = self.scraper.get_session(self.scraper.base_url)
+        self.assertEquals(session.status_code, 200)
+
+    def test_query_laptop_segment(self):
+        products = self.scraper.query_laptop_segment()
+        self.assertEquals(len(products) > 0, True)
+
+    # def test_product_data(self):
+    #     process = self.scraper.process()
+    #     self.assertEquals(process, True)
