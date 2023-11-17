4001dd5f34816cb898a9c76c24df62dfb039e23f
sgs22 <samuel_smith@outlook.com>
2023-11-17 00:54:28 +0000

    add new scraper model

--- scraper/models.py
+++ scraper/models.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 
 
 class Product(models.Model):
-    name = models.CharField()
-    price = models.FloatField()
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     product_code = models.CharField(max_length=255, blank=True, null=True)
-    description = models.TextField()
+    description = models.TextField(max_length=5000)
     additional_info = models.JSONField(blank=True, null=True)
-    image = models.ImageField(upload_to='products', blank=True, null=True)
+    # image = models.ImageField(upload_to='products', blank=True, null=True)
     image_urls = models.JSONField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -20,13 +20,48 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
-    
+
+    @classmethod
+    def create_from_product_data(cls, product_data: dict):
+        if not cls.valid_product_data(product_data):
+            return None
+        create_product = cls.objects.create(
+            name=product_data.get('name'), price=product_data.get('price', 0),
+            product_code=product_data.get('product_code', ''),
+            description=product_data.get('description', ''),
+            additional_info=product_data.get('additional_info', {}),
+            image_urls=product_data.get('image_urls', {})
+        )
+        if create_product:
+            return create_product
+        return None
+
+    @classmethod
+    def valid_product_data(cls, product_data: dict) -> bool:
+        valid = True
+        required_fields = ['name', 'price']
+        for k, v in product_data:
+            if not k in required_fields:
+                valid = False
+        return valid
+
+    @classmethod
+    def get_model_fields(cls) -> list:
+        fields = cls._meta.fields
+        field_names = [field.name for field in fields]
+        return field_names
+
+
 class CurrysScraper:
     def __init__(self):
         self.base_url = 'https://www.currys.co.uk'
         self.session = requests.Session()
-        self.session.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+        self.session.headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        }
 
     def search_products(self, search_query):
         search_query = search_query.replace(' ', '+')
@@ -45,21 +80,152 @@ class CurrysScraper:
             products.append({'name': name, 'price': price})
 
         return products
-    
-    def get_product_details(self, product_code: str):
-        url = f'https://www.currys.co.uk/search?q={product_code}'
 
-        response = self.session.get(url)
+    def query_product_segment(self):
+        url = "https://www.currys.co.uk/computing/laptops/laptops?start=0&sz=5&viewtype=listView"
+        soup = self.get_soup_session(url)
+        products = []
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-        print(soup)
-        product = {}
-        product_name = soup.find('h1', class_='product-name').text.strip()
-        product_price = soup.find('span', class_='value').text.strip()
-        product.append({'name': product_name, 'price': product_price})
-        print(product)
+        for product in soup.find_all("div", class_="product-tile  "):
+            name = product.find('h2', class_='pdp-grid-product-name')
+            link = product.find('a', class_='link text-truncate pdpLink')
+            href = link['href']
+            products.append({'name': name, 'url': href})
+        return products
+
+    def get_product_details_from_list(self, products: list):
+        for item in products:
+            response = self.get_soup_session(item.get('url'))
+            response.find_all()
+
+    def get_soup_session(self, url: str) -> BeautifulSoup:
+        session = self.session.get(url)
+        soup = BeautifulSoup(session.content, 'html.parser')
+        return soup
+
+    def get_session(self, url: str):
+        session = self.session.get(url)
+        return session
 
 
-# if __name__ == "__main__":
-#     scraper = CurrysScraper()
-#     scraper.get_product_details('359445')
+class ProductScraper:
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+        self.session = requests.Session()
+        self.session.headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        }
+
+    def search_products(self, search_query, endpoint: str):
+        search_query = search_query.replace(' ', '+')
+        url = f"{self.base_url}{endpoint}"
+
+        soup = self.get_soup_session(url)
+        products = []
+
+        for product in soup.find_all('article', class_='product'):
+            name = product.find('strong', class_='productTitle').text.strip()
+            price = product.find('strong', class_='current-price').text.strip()
+            products.append({'name': name, 'price': price})
+
+        return products
+
+    def query_product_segment(self, url: str):
+        soup = self.get_soup_session(url)
+        products = []
+
+        for product in soup.find_all("div", class_="product-tile  "):
+            name = product.find('h2', class_='pdp-grid-product-name')
+            link = product.find('a', class_='link text-truncate pdpLink')
+            href = link['href']
+            products.append({'name': name, 'url': href})
+        return products
+
+    def get_product_details_from_list(self, products: list):
+        for item in products:
+            response = self.get_soup_session(item.get('url'))
+            response.find_all()
+
+    def get_soup_session(self, url: str) -> BeautifulSoup:
+        session = self.session.get(url)
+        soup = BeautifulSoup(session.content, 'html.parser')
+        return soup
+
+    def get_session(self, url: str):
+        session = self.session.get(url)
+        return session
+
+    def paginate(self, per_page: int, total: int):
+        pages = []
+        for i in range(0, total, per_page):
+            pages.append(i)
+        return pages
+
+
+class LaptopsDirectScraper(ProductScraper):
+    def __init__(self):
+        super().__init__('https://www.laptopsdirect.co.uk')
+
+    def query_laptop_segment(self) -> list:
+        url = 'https://www.laptopsdirect.co.uk/ct/laptops-and-netbooks/laptops?pageNumber=1'
+        # pages = self.paginate(48, 954)
+        page_blocks = self.paginate(48, 50)
+        page = 1
+        products = []
+        for page in page_blocks:
+            print(f'processing product range: {page_blocks}')
+            url = f'https://www.laptopsdirect.co.uk/ct/laptops-and-netbooks/laptops?pageNumber={page}'
+            soup = self.get_soup_session(url)
+            product_list = soup.find_all(class_='OfferBox')
+            for product in product_list:
+                name = product.find('a', class_='offerboxtitle').text.strip()
+                link = product.find('a', class_='offerboxtitle')['href']
+                products.append(
+                    {'product_name': name, 'product_url': f'{self.base_url}{link}'})
+            page += 1
+        print(len(products))
+        print(products)
+        return products
+
+    def get_product_details(self, product_url: str):
+        soup = self.get_soup_session(product_url)
+        name = soup.find('span', class_='title')
+        price = soup.find('span', class_='VersionOfferPrice')
+        price = price.find('img')['alt']  # type: ignore
+        product_code = soup.find(
+            'span', class_='sku text-grey margin-right-15')
+        description = soup.find_all('div', class_='ProductDescription')
+        additional_info = soup.find('div', class_='specData')
+        image_urls = soup.find('a', class_='fancyboxThumb')
+        if image_urls:
+            image_urls = image_urls['href']  # type: ignore
+        product_data = {'name': name, 'price': price,
+                        'product_code': product_code,
+                        'description': description,
+                        'additional_info': {'info': additional_info},
+                        'image_urls': [image_urls]
+                        }
+        print('product_data', product_data)
+        return product_data
+
+    def create_product(self, product_data: dict):
+        create_product = Product.create_from_product_data(product_data)
+        if create_product:
+            return True
+        return False
+
+    def process(self):
+        products = self.query_laptop_segment()
+        if products:
+            for product in products:
+                product_url = product.get('product_url')
+                if product_url:
+                    product_data = self.get_product_details(product_url)
+                    if product_data:
+                        success = self.create_product(product_data)
+                        print('successfully created') if success else print(
+                            'fail creating product')
+        return True
