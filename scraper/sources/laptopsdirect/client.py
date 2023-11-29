5ee4675d63ea6e961b10f7d99a8a1f3eea052bd5
sgs22 <samuel_smith@outlook.com>
2023-11-29 13:43:41 +0000

    clear up scraper client

--- scraper/sources/laptopsdirect/client.py
+++ scraper/sources/laptopsdirect/client.py
@@ -7,14 +7,12 @@ class LaptopsDirectScraper(ProductScraper):
         super().__init__('https://www.laptopsdirect.co.uk')
 
     def query_laptop_segment(self) -> list:
-        url = 'https://www.laptopsdirect.co.uk/ct/laptops-and-netbooks/laptops?pageNumber=1'
-        # pages = self.paginate(48, 954)
+        url = f'{self.base_url}/ct/laptops-and-netbooks/laptops?pageNumber=1'
         page_blocks = self.paginate(48, 50)
         page = 1
         products = []
         for page in page_blocks:
-            print(f'processing product range: {page_blocks}')
-            url = f'https://www.laptopsdirect.co.uk/ct/laptops-and-netbooks/laptops?pageNumber={page}'
+            url = f'{self.base_url}/ct/laptops-and-netbooks/laptops?pageNumber={page}'
             soup = self.get_soup_session(url)
             product_list = soup.find_all(class_='OfferBox')
             for product in product_list:
@@ -23,30 +21,61 @@ class LaptopsDirectScraper(ProductScraper):
                 products.append(
                     {'product_name': name, 'product_url': f'{self.base_url}{link}'})
             page += 1
-        print(len(products))
-        print(products)
         return products
 
-    def get_product_details(self, product_url: str):
+    def get_product_details(self, product_url: str) -> dict:
         soup = self.get_soup_session(product_url)
-        name = soup.find('span', class_='title')
+        name = soup.find('span', class_='title').text  # type: ignore
         price = soup.find('span', class_='VersionOfferPrice')
-        price = price.find('img')['alt']  # type: ignore
+        price_img = price.find('img') if price else None
+        if price_img:
+            price = price_img['alt'].replace('£', '')  # type: ignore
         product_code = soup.find(
-            'span', class_='sku text-grey margin-right-15')
-        description = soup.find_all('div', class_='ProductDescription')
-        additional_info = soup.find('div', class_='specData')
+            'span', class_='sku text-grey margin-right-15').text  # type: ignore
         image_urls = soup.find('a', class_='fancyboxThumb')
         if image_urls:
             image_urls = image_urls['href']  # type: ignore
-        product_data = {'name': name, 'price': price,
-                        'product_code': product_code,
-                        'description': description,
-                        'additional_info': {'info': additional_info},
-                        'image_urls': [image_urls]
-                        }
-        print('product_data', product_data)
+            image_urls = f'{self.base_url}{image_urls}'  # type: ignore
+        description = soup.find('div', class_='ProductDescription')
+        additional_info = soup.find('div', id='specs')
+        if additional_info:
+            # get tbody
+            additional_info_data = []
+            table_body = additional_info.find('tbody')
+            # for each tr get td[0] and td[1]
+            # create list with all tr elements from table_body and split each element in a list
+            # type: ignore
+            table_row = [tr.find_all('td') for tr in table_body.find_all('tr')]
+            # for each tr get td[0] and td[1] and make t[0] the key and td[1] the value
+            for data in table_row:
+                key = data[0].text.strip()
+                print('key', key)
+                value = data[1].text.strip()
+                print('value', value)
+                additional_info_data.append({key: value})
+        else:
+            additional_info_data = []
+        product_data = {
+            'name': name,
+            'price': price,
+            'product_code': product_code,
+            'description': description,
+            'additional_info': {'info': additional_info_data},
+            'image_urls': [image_urls]
+        }
         return product_data
+
+    def query_product_page(self, page: int) -> list[dict]:
+        url = f'{self.base_url}/ct/laptops-and-netbooks/laptops?pageNumber={page}'
+        soup = self.get_soup_session(url)
+        product_list = soup.find_all(class_='OfferBox')
+        products = []
+        for product in product_list:
+            name = product.find('a', class_='offerboxtitle').text.strip()
+            link = product.find('a', class_='offerboxtitle')['href']
+            products.append(
+                {'product_name': name, 'product_url': f'{self.base_url}{link}'})
+        return products
 
     def create_product(self, product_data: dict):
         create_product = Product.create_from_product_data(product_data)
@@ -66,3 +95,16 @@ class LaptopsDirectScraper(ProductScraper):
                         print('successfully created') if success else print(
                             'fail creating product')
         return True
+
+    def process_product_page(self) -> list[dict]:
+        product_schemas = []
+        products = self.query_product_page(1)
+        for product in products:
+            print(
+                f'checking product {product.get("product_name")} with url {product.get("product_url")}')
+            url = product.get('product_url')
+            if url:
+                product_data = self.get_product_details(url)
+                if product_data:
+                    product_schemas.append(product_data)
+        return product_schemas
