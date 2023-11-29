a427f18fe3337875987096b83e688e1e1a43d43d
sgs22 <samuel_smith@outlook.com>
2023-11-29 13:43:22 +0000

    increase model description max length

--- scraper/models.py
+++ scraper/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     product_code = models.CharField(max_length=255, blank=True, null=True)
-    description = models.TextField(max_length=5000)
+    description = models.TextField(max_length=10000)
     additional_info = models.JSONField(blank=True, null=True)
     image_urls = models.JSONField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
