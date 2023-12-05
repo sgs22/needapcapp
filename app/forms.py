360ceddf6c631b3a12133817ea9ac7487fe343b0
sgs22 <samuel_smith@outlook.com>
2023-12-05 20:38:27 +0000

    update form template

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,11 @@ class QuizForm(forms.Form):
     def __init__(self, *args, **kwargs):
         questions = kwargs.pop('questions')
         super().__init__(*args, **kwargs)
-        for question in questions:
+        for i, question in enumerate(questions):
             self.fields[question.question] = forms.ChoiceField(
-                choices=[(choice.choice, choice.choice) for choice in question.choice_set.all()], 
-                widget=forms.RadioSelect(attrs={'class': 'text-2xl'})
+                choices=[(choice.choice, choice.choice)
+                         for choice in question.choice_set.all()],
+                widget=forms.RadioSelect(
+                    attrs={'class': 'text-2xl', 'id': f'question_{i}'}),
+
             )
